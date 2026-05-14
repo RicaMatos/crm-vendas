3c4df855cd03b950c3d15e7205859ddf239d8457
Ricardo <ricardomatos@gmail.com>
2026-05-13 22:00:30 -0300

    security: otimiza configuração CSP e ajusta middleware

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -1,19 +1,7 @@
 const helmet = require('helmet');
 
 const securityMiddleware = helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "https:"],
-      connectSrc: ["'self'", process.env.SUPABASE_URL || ''],
-      fontSrc: ["'self'"],
-      objectSrc: ["'none'"],
-      mediaSrc: ["'self'"],
-      frameSrc: ["'none'"]
-    }
-  },
+  contentSecurityPolicy: false,
   crossOriginEmbedderPolicy: false,
   hsts: {
     maxAge: 31536000,
@@ -26,4 +14,27 @@ const securityMiddleware = helmet({
   xPermittedCrossDomainPolicies: 'none'
 });
 
-module.exports = securityMiddleware;
+const cspMiddleware = (req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-hashes' https://unpkg.com https://cdn.jsdelivr.net https://accounts.google.com https://apis.google.com; " +
+    "script-src-attr 'self' 'unsafe-inline' 'unsafe-hashes'; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "style-src-attr 'self' 'unsafe-inline'; " +
+    "img-src 'self' data: https: http:; " +
+    "connect-src 'self' " + (process.env.SUPABASE_URL || '') + " https://*.supabase.co https://*.googleusercontent.com; " +
+    "font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com data:; " +
+    "object-src 'none'; " +
+    "media-src 'self'; " +
+    "frame-src 'none' https://accounts.google.com; " +
+    "frame-ancestors 'none';"
+  );
+  next();
+};
+
+module.exports = (req, res, next) => {
+  securityMiddleware(req, res, () => {
+    cspMiddleware(req, res, next);
+  });
+};
